146904f313946810eab05d289c7e06e25c467266
trkhmd <78960532+trkhmd@users.noreply.github.com>
2022-10-04 19:23:53 +0200

    Ajout springBoot

---
+++ wemeet/src/main/java/com/example/wemeet/WemeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.wemeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemeetApplication.class, args);
+	}
+
+}
